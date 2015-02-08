47138e5eecf6e58c144a30616200644d5a76c686
Alan Matthews <matthewsa1320@gmail.com>
2015-02-08 17:45:45 -0500

    Almost an etsy page

--- app/controllers/etsy_controller.rb
+++ app/controllers/etsy_controller.rb
@@ -1,17 +1,33 @@
 class EtsyController < ApplicationController
-  ETSY_API_TOKEN = ENV['ETSY_API_TOKEN']
+  
     
   def show
-		Etsy.api_key = ETSY_API_TOKEN
 		@results = nil
 		if params[:q]
-			resp = Etsy::Request.get('listing/active',
-															 :include => ['Images', 'Shop'],
+			resp = Etsy::Request.get('/listings/active',
+															 :includes => ['Images', 'Shop'],
 															 :keywords => params[:q])
+			#binding.pry
 			@results = JSON.parse(resp.body)['results']
 		end
 		render :show
 	end
+
+
+	# def show
+	# 	@results = nil
+ #    Etsy.api_key = ETSY_API_TOKEN
+	# 	resp = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => 'beer')
+	#   @results = JSON.parse(resp.body)['results']
+	# end
+	# puts @results
+
+	# def show
+	# 	@results = nil
+	# 	Etsy.api_key = ETSY_API_TOKEN
+	# 	@results = Etsy::Listing.find(217378316)
+	# end
+	# puts @results
 end
 
 
